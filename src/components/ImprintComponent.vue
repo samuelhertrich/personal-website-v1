8413d3de68462708f0393ee6c48e85781fa9ed26
samuelhertrich <samuhertrich198@gmail.com>
2022-05-15 20:32:46 +0200

    added imprint

---
+++ src/components/ImprintComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="imprint">
+      <section>
+          <h1><span>></span>imprint</h1>
+          <div>
+              <p>
+                Samuel Hertrich<br />
+                Theoderichstr. 3<br />
+                85051 Ingolstadt
+            </p>
+          </div>
+      </section>
+      <section>
+          <h1><span>></span>contact</h1>
+          <div>
+              <p>
+                phone: +49 (0) 151 728 615 98<br />
+                email: contact@samuelhertrich.com
+            </p>
+          </div>
+      </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'ImprintComponent',
+}
+</script>
---
+++ src/views/ImprintView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <ImprintComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ImprintComponent from '@/components/ImprintComponent.vue'
+
+export default {
+  name: 'ImprintView',
+  components: {
+    ImprintComponent
+  }
+}
+</script>
