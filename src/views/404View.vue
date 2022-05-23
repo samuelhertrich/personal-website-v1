eaa686aae9df48489efdfdfc1f59abf5d6676747
samuelhertrich <samuhertrich198@gmail.com>
2022-05-23 16:42:37 +0200

    added 404View

---
+++ src/views/404View.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="not-found">
+        <div>404</div>
+        <p>looks like the site doesn't exist.</p>
+    </section>
+</template>
+
+<script>
+export default {
+  name: '404View',
+}
+</script>
+
+<style>
+
+.not-found {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.not-found div {
+    font-size: 5rem;
+    font-family: var(--secondary-font);
+    color: var(--secondary-color);
+}
+
+</style>
